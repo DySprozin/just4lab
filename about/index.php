4cb46aef5a9503293a99b80d28c40115400295ff
DySprozin <dysprozin@yandex.ru>
2016-09-21 00:56:48 +0300

    about

---
+++ about/index.php
@@ -0,0 +1,62 @@
+<?php
+define ('ROOT', $_SERVER['DOCUMENT_ROOT'] . '/');
+include ROOT . 'conf/main_conf.php';
+
+include ROOT . 'inc/header_common.htm';
+?>
+
+
+
+<br class="break0">
+
+<table class="table_actual">
+ <tr>
+  <td class="border_left_top">
+   <div class="actual_title">
+   </div>
+  </td>
+  <td class="paper_right_top">&nbsp;
+  </td>
+ </tr>
+ <tr>
+  <td class="paper_left">
+   <div class="actual_body paper_body">
+ Мы — небольшая группа единомышленников, что решили попробовать себя в весьма интересном деле — производство в России. Ниже представлена основная информация о компании. Реквизиты можно скачать <a href="https://elba.kontur.ru/card/fpd6f2mz5i" target="_blank">по этой ссылке</a>.
+<br><br><b>Способы оплаты:</b><br>
+<i>Для юридических лиц</i> — оплата безналом: мы выставляем счет, вы его оплачиваете и получаете товар.<br>
+<i>Для физических лиц</i> — оплата банковской картой, либо через Яндекс.Деньги (появится в скором времени)<br>
+<br>
+<b>Доставка:</b><br>
+Курьерская доставка: 300 руб. Другие варианты также обсуждаемы (в т.ч. самовывоз).<br><br>
+<b>Контакты:</b><br>
+На данный момент доступен только Email, в скором времени появится телефон для связи:<br>
+manager@4lab.su
+   </div>
+  </td>
+  <td class="paper_right">
+&nbsp;
+  </td>
+ </tr>
+ <tr>
+  <td class="border_left_bottom">
+   &nbsp;
+  </td>
+  <td class="border_right_bottom">&nbsp;
+  </td>
+ </tr>
+</table>
+<div class="footer_ie">&nbsp;</div>
+<!-- footer -->
+<?php include ROOT . 'inc/footer_common.htm'; ?>
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ components/about/header_about_index.php
@@ -0,0 +1,11 @@
+<?php
+  $meta['title'] = 'О компании';
+  $meta['description'] = 'О компании';
+  $meta['keywords'] = '4lab.su';
+  $meta['css'] = '/css/common.css';
+  $meta['head'] = ' &nbsp;<br>
+    Лучше зажечь свечу, чем проклинать тьму...&nbsp;<br>
+    &nbsp;<br>
+   &nbsp;
+  ';
+  $meta['menu'] = '';
