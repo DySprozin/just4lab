5d0ada3d52da54139167739c4649f2c8e1791987
DySprozin <dysprozin@yandex.ru>
2016-10-11 06:14:41 +0300

    ttt

---
+++ components/money/header_money_fail.php
@@ -0,0 +1,11 @@
+<?php
+  $meta['title'] = 'Ой! Печалька...';
+  $meta['description'] = '';
+  $meta['keywords'] = '';
+  $meta['css'] = '/css/common.css';
+  $meta['head'] = ' &nbsp;<br>
+    &nbsp;<br>
+    &nbsp;<br>
+   &nbsp;
+  ';
+  $meta['menu'] = '';
